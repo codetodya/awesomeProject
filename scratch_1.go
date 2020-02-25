03ad1cfd71352993820a4977e767b89e116be7be
sumit.mahamuni <sumit.mahamuni@go-jek.com>
2020-02-25 23:38:21 +0530

    Adding more examples for arrays.

--- scratch_1.go
+++ scratch_1.go
@@ -33,10 +33,24 @@ func main() {
 	fmt.Printf("as[0].Name: %s\n", as[0].Name)
 	fmt.Printf("as[1].Name: %s\n", as[1].Name)
 	fmt.Printf("as[2].Name: %s\n", as[2].Name)
+
+	areTheNamesReallyChanging(as)
+
+	fmt.Printf("length of as: %d\n", len(as))
+	fmt.Printf("as[0].Name: %s\n", as[0].Name)
+	fmt.Printf("as[1].Name: %s\n", as[1].Name)
+	fmt.Printf("as[2].Name: %s\n", as[2].Name)
 }
 
 func letChangeTheNames(bs []A) {
 	for i, a := range bs {
 		a.Name = fmt.Sprintf("a%d got a new Name, Hurray!!", i+1)
+	}
+}
+
+
+func areTheNamesReallyChanging(bs []A) {
+	for i, _ := range bs {
+		bs[i].Name = fmt.Sprintf("a%d getting a new Name?", i+1)
 	}
 }
--- scratch_2.go
+++ scratch_2.go
@@ -33,10 +33,24 @@ func main() {
 	fmt.Printf("as[0].Name: %s\n", as[0].Name)
 	fmt.Printf("as[1].Name: %s\n", as[1].Name)
 	fmt.Printf("as[2].Name: %s\n", as[2].Name)
+
+	iThinkYouGotItNow(as)
+
+	fmt.Printf("length of as: %d\n", len(as))
+	fmt.Printf("as[0].Name: %s\n", as[0].Name)
+	fmt.Printf("as[1].Name: %s\n", as[1].Name)
+	fmt.Printf("as[2].Name: %s\n", as[2].Name)
 }
 
 func wonderWhatIsGoingToHappenNow(bs []*A) {
 	for i, a := range bs {
 		a.Name = fmt.Sprintf("will a%d get a new Name? :thinking:", i+1)
+	}
+}
+
+
+func iThinkYouGotItNow(bs []*A) {
+	for i, _ := range bs {
+		bs[i].Name = fmt.Sprintf("a%d is getting a new Name! :banana dance:", i+1)
 	}
 }
